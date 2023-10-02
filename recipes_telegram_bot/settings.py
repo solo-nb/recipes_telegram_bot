84c3ad5aed5e51e7e5dffd87196bf907bb7e39be
solo-nb <to.solo.nb@gmail.com>
2023-10-02 17:48:22 +0500

    add allowed_host and secret_key into env values

--- recipes_telegram_bot/settings.py
+++ recipes_telegram_bot/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0jevlc28*2v)_%f(8#uasr6tqtgk(g46$)&*+g$#d!#71x6%bh'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
 # Application definition
