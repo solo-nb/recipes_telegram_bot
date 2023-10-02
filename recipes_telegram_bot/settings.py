f42c4652160e2c612f335f0397ebe9e674735dcf
solo-nb <to.solo.nb@gmail.com>
2023-10-02 21:03:28 +0500

    fix ALLOWED_HOSTS in settings

--- recipes_telegram_bot/settings.py
+++ recipes_telegram_bot/settings.py
@@ -30,9 +30,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-print(ALLOWED_HOSTS)
-
+try:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+except AttributeError:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
